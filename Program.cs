f941437dd3652deac19d87ce86b795a2c6a37015
KalamanovaElena <alia13011988@yandex.ru>
2023-01-11 13:35:51 +0300

    Задача 2 семинара 1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,26 @@
-﻿Console.WriteLine("Введи число: ");
-int num1 = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введи число: ");
-int num2 = int.Parse(Console.ReadLine()!);
-if (num1 > num2) 
-	{Console.WriteLine($"Большее число {num1}, меньшее число {num2}");}
-    else if (num2 > num1)
-		{Console.WriteLine($"Большее число {num2}, меньшее число {num1}");}
+﻿// Console.WriteLine("Введи число: ");
+// int num1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи число: ");
+// int num2 = int.Parse(Console.ReadLine()!);
+// if (num1 > num2) 
+// 	{Console.WriteLine($"Большее число {num1}, меньшее число {num2}");}
+//     else if (num2 > num1)
+// 		{Console.WriteLine($"Большее число {num2}, меньшее число {num1}");}
+
+//2 задача 1 семинара
+// Console.WriteLine("Введи число: ");
+// int num1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи число: ");
+// int num2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введи число: ");
+// int num3 = int.Parse(Console.ReadLine()!);
+// int max = num1;
+// if (max < num2) {
+//     max = num2;
+// }
+// if (max < num3) {
+//     max = num3;
+// }
+// Console.WriteLine ($"Число {max} является максимальным");
+
+// Задача 3 семинара 1
