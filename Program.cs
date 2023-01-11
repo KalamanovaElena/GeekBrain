3516eaad994ac93f78ac3e0bedf21e0cd06b3677
KalamanovaElena <alia13011988@yandex.ru>
2023-01-11 12:57:45 +0300

    Первая задача 1 семинара

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введи число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введи число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+if (num1 > num2) 
+	{Console.WriteLine($"Большее число {num1}, меньшее число {num2}");}
+    else if (num2 > num1)
+		{Console.WriteLine($"Большее число {num2}, меньшее число {num1}");}
